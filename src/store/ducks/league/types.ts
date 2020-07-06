99719ef81c8bbb7fb7a016b296fa1e2e568460ee
Thiago Diniz <thiago.diniz@apllos.com>
2020-07-06 16:50:15 -0300

    Creating League interfaces

---
+++ src/store/ducks/league/types.ts
@@ -0,0 +1,50 @@
+import { Player } from "../players/types";
+
+/**
+ * League Action Types
+ */
+export enum Types {
+    LOAD_LEAGUE = 'league/LOAD_LEAGUE',
+    LOAD_LEAGUE_SUCCESS = 'league/LOAD_LEAGUE_SUCCESS',
+    LOAD_LEAGUE_FAILURE = 'league/LOAD_LEAGUE_FAILURE',
+}
+
+
+/**
+ * League
+ */
+export interface League {
+    id: number;
+    name: string;
+    status: number;
+    register_date: Date;
+    divisions: Division[];
+}
+
+export interface Division {
+    idLeagueDivision: number;
+    division: string;
+    players: DivisionPlayer[];
+}
+
+export interface DivisionPlayer extends Player {
+    idLeagueDivisionPlayer: number;
+    stats: PlayerStats;
+}
+
+export interface PlayerStats {
+    completedMatches: number;
+    points: number;
+    wins: number;
+    draws: number;
+    loses: number;
+    scoredGoals: number;
+    concededGoals: number;
+    goalDifference: number;
+}
+
+export interface LeagueState {
+    readonly league: League;
+    readonly loading: boolean;
+    readonly error: boolean;
+ }
