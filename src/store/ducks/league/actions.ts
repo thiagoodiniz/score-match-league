8df3c503e91e25985eb78f99a005a37802cd2656
Thiago Diniz <thiago.diniz@apllos.com>
2020-07-06 16:52:51 -0300

    League actions

---
+++ src/store/ducks/league/actions.ts
@@ -0,0 +1,8 @@
+import { action } from 'typesafe-actions';
+import { Types, League } from './types';
+
+export const loadLeague = () => action(Types.LOAD_LEAGUE);
+
+export const loadLeagueSuccess = (league: League) => action(Types.LOAD_LEAGUE_SUCCESS, league);
+
+export const loadLeagueFailure = () => action(Types.LOAD_LEAGUE_FAILURE);
