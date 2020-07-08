ec137372804dea53996e6784446ae453d5499363
Thiago Diniz <thiago.diniz@apllos.com>
2020-07-08 10:44:03 -0300

    structure to show League, Divison and Matches

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Container } from './styles';
+import League from '../League';
 
 const Home = () => {
     return(
         <Container>
-            Hello
+            <League />
         </Container>
     );
 }
